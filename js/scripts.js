f8c6f8e4c842cb51fde25af73567565a89496f86
marynganga <marynganga.mw@gmail.com>
2017-08-25 19:05:53 +0300

    1. Add code to add the dice roll number to the turn total.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,30 @@
+//Global Variables
+var player1, player2;
 //Back-End Logic
 //Constructor function for a player
-function Player(name, turnTotal, totalScore) {
+function Player(name, turnTotal, overallScore) {
     this.name = name;
     this.turnTotal = 0;
-    this.totalScore = 0;
+    this.overallScore = 0;
 }
 
 //Funtion on what is to happen when the dice is rolled.
 Player.prototype.roll = function () {
     var randomNo = Math.floor((Math.random() * 6) + 1);
-    return randomNo;
-    this.turnTotal += randomNo;
-    this.turnTotal;
-    
-//     if (randomNo > 1) {
-//     this.totalScore += this.diceRoll;
-//      console.log(this.totalScore);
-//  } else {
-//      this.totalScore = 0;
-//     console.log(this.totalScore);
-//    alert("Oops you got a 1. Your turn is over");
-//    
-//};
+   
+         if (randomNo === 1) {
+              this.turnTotal = 0;
+         console.log(this.turnTotal);
+        return alert("Oops you got a 1. Your turn is over");
+      } else {
+          this.turnTotal += randomNo;
+          console.log(this.turnTotal);
+    };
+     return randomNo;
 };
-//function findTotalScore(){
-//    if (randomNo > 1) {
-//        this.totalScore += this.diceRoll;
-//        alert(this.totalScore);
-//        return this.totalScore;
-//        console.log(this.totalScore);
-//    } else {
-//        this.totalScore = 0;
-//        return this.totalScore;
-//        console.log(this.totalScore);
-//        alert("Oops you got a 1. Your turn is over.");
-//
-//    }
+//Function on what is to happen when a player holds the game.
+//Player.prototype.hold = function(){
+//    this.overallScore += turnTotal;
 //}
 
 //Reset the fields
@@ -61,29 +50,26 @@ $(document).ready(function () {
         var gamer2 = $("#player2Name").val();
         console.log(gamer2);
         //Put the names into an object using the constructor Players
-        var player1 = new Player(gamer1);
-        var player2 = new Player(gamer2);
+        player1 = new Player(gamer1);
+        player2 = new Player(gamer2);
         console.log(player1, player2);
         //Output the names into each appropriate section
         $(".player1NameOutput").text(player1.name);
         $(".player2NameOutput").text(player2.name);
         //Clear the input fields
         resetFields();
-        //});
+        });
 
         $('.roll').click(function (event) {
             event.preventDefault();
             //call the function to generate random numbers
-            //display the turn score
-     $('.diceroll1').text(player1.roll());
-        console.log(player1.roll());
-
-            //display the total score
-//            $('.totalScore1').text(player1.totalScore);
-//         console.log(player1.totalScore);
-      });
-
-    });
+            //display the rolled dice number
+            $('.diceroll1').text(player1.roll());
+            //display the turn score (temporary score)
+            $('.turnScore1').text(player1.turnTotal);
+            //display the overall score            $('.overralScore1').text(player1.totalScore);
+    
+        });
 
 
 });
