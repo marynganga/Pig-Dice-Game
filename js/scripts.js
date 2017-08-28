52c093e91f719bf88ebbd38c84a4322a69c381fb
marynganga <marynganga.mw@gmail.com>
2017-08-28 09:46:50 +0300

    1.Add an if else if statement in the .roll() prototype to disable a playing area when dice roll is 1.

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,17 @@ function Player(name, turnTotal, diceRoll, overallScore, active) {
 }
 //Function to disable and enable gaming areas according to which player is active.
 function activeUser() {
-    if (player1.active === true || player2.active === false) {
+    if (player1.active === true && player2.active === false) {
         $('.player1Area').children().prop('disabled', false);
         $('.player1Area').removeClass('disableGamingArea');
         $('.player2Area').children().prop('disabled', true);
         $('.player2Area').addClass('disableGamingArea');
-
-    } else {
+    } else { 
         $('.player1Area').children().prop('disabled', true);
         $('.player1Area').addClass('disableGamingArea');
         $('.player2Area').children().prop('disabled', false);
         $('.player2Area').removeClass('disableGamingArea');
-    };
+    }
 };
 //Funtion on what is to happen when the dice is rolled.
 Player.prototype.roll = function () {
@@ -31,8 +30,23 @@ Player.prototype.roll = function () {
     if (randomNo === 1) {
         this.turnTotal = 0;
         this.diceRoll = 1;
-        this.active = false;
-        activeUser();
+        if (this.active === player1.active) { //disable and enable gaming areas when dice is a 1 according to which player is active.
+            player1.active = false;
+            player2.active = true;
+            $('.player1Area').children().prop('disabled', true);
+            $('.player1Area').addClass('disableGamingArea');
+            $('.player2Area').children().prop('disabled', false);
+            $('.player2Area').removeClass('disableGamingArea');
+        } else if (this.active === player2.active) {
+            player2.active = false;
+            player1.active = true;
+            $('.player2Area').children().prop('disabled', true);
+            $('.player2Area').addClass('disableGamingArea');
+            $('.player1Area').children().prop('disabled', false);
+            $('.player1Area').removeClass('disableGamingArea');
+        }else{
+            console.log("not working");
+        }
         return alert("Oops you got a 1. Your turn is over!");
     } else {
         this.turnTotal += randomNo;
